0867b1fe35538d7756b625b91e255650eb72401e
Chandler R <chandler.renteria@gmail.com>
2020-11-14 14:27:14 -0700

    fixed responsiveness, fixed formating with gallery and contact sections.

--- contact.php
+++ contact.php
@@ -1,42 +1,45 @@
-<html>
-    <body>
-        <?php 
-            $firstname = $lastname = $email = $company = "";
-            $nameErr = $emailErr = $companyErr = "Required Field"
-            if ($_SERVER["REQUEST_METHOD" == "POST"]) {
-                if (empty($_POST["firstName"])) {
-                $nameErr = "Name is required";
-                } else {
-                $name = testInput($_POST["firstName"]);
-                }
 
-                if ($_SERVER["REQUEST_METHOD" == "POST"]) {
-                if ($_POST["lastName"]) {
-                    $lastname = testInput($_POST["lastName"]);
-                }
-                
-                if (empty($_POST["email"])) {
-                $emailErr = "Email is required";
-                } else {
-                $email = testInput($_POST["email"]);
-                }
-                
-                if (empty($_POST["company"])) {
-                $companyErr = "";
-                } else {
-                $company = testInput($_POST["company"]);
-                }
-            }
-            function testInput($data) {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
-            }
-            
-            echo "Thank you, an email has been sent confirming your submission."
+<?php 
+    $firstname = $lastname = $email = $company = "";
+    $nameErr = $emailErr = $companyErr = "Required Field";
+    if ($_SERVER["REQUEST_METHOD" == "POST"]) {
+        if (empty($_POST["firstName"])) {
+        $nameErr = "Name is required";
+        } else {
+        $name = testInput($_POST["firstName"]);
+        }
 
+        if ($_SERVER["REQUEST_METHOD" == "POST"]) {
+        if ($_POST["lastName"]) {
+            $lastname = testInput($_POST["lastName"]);
+        }
+        
+        if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+        } else {
+        $email = testInput($_POST["email"]);
+        }
+        
+        if (empty($_POST["company"])) {
+        $companyErr = "";
+        } else {
+        $company = testInput($_POST["company"]);
+        }
+    }
 
-        ?>
-    </body>
-</html>
+    function testInput($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    alert("Thank you, an email has been sent confirming your submission.");
+
+    $to = ($email, "chandler.renteria@gmail.com");
+    $subject = ("New Employment Submission!");
+    $body = ("$firstname $lastname from $company is interested in speaking with you. Email: $email.")
+    mail($to, $subject, $body)
+
+?>
+
